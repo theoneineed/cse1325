1928ac7df54fce643f177ccad95bd761e85d7d72
Nabin <nabinbabu2015@gmail.com>
2020-02-10 19:20:58 -0600

    Feb 10 update

---
+++ P03/Color.cpp
@@ -0,0 +1,25 @@
+#include "Color.h"
+
+Color::Color (int red, int green, int blue):_red{red},_green{green},_blue{blue}{}
+
+std::string Color::to_string()
+{
+  std::string data= "(" +std::to_string(_red)+","+std::to_string(_green)+","+std::to_string(_blue)+")";
+  return data;
+}
+
+std::string Color::colorize(std::string text)
+{
+  std::stringstream g;
+
+  g<<"\033[38;2;"
+  <<_red<<";"
+  <<_green<<";"
+  <<_blue
+  <<"m"//I am using prof's notes as reference here
+  <<text//Text to print in the color
+  <<"\033[0m"; //Resets terminal to default state
+  //<<std::endl;  //Newline and flushes the output buffer
+
+  return g.str();
+}
---
+++ P03/Color.h
@@ -0,0 +1,26 @@
+#ifndef __COLOR_H
+#define __COLOR_H
+
+#include <iostream>
+#include <string>
+#include <sstream> //got it from geeksforgeeks
+class Color
+{
+private:
+  int _red,_green,_blue;
+public:
+  Color(int red, int green, int blue);
+  //Since all the colors will be combination of rgb
+  std::string to_string();
+  //method returns a string representation of the RGB
+  std::string colorize(std::string text);
+  //method returns its parameter preceded by the ANSI escape code for set fg color,and followed by ANSI esc code for Reset
+};
+
+
+
+
+
+
+
+#endif
