401a8767c163eded5b8db3b84549d4076d83e48a
Nabin <nabinbabu2015@gmail.com>
2020-02-18 02:01:49 -0600

    ...

--- P04/coin full credit.cpp
+++ P04/coin full credit.cpp
@@ -1,3 +1,9 @@
+
+
+#include <iostream>
+#include <string>
+#include <sstream> //got it from geeksforgeeks
+
 class Coin
 {
 private:
@@ -5,7 +11,11 @@ private:
   Year _year;
   string* _notes;
 public:
-  Coin(size:Size, year:Year)
-  Coin (rhs:const Coin&)
-  operator
-}
+  Coin(size:Size, year:Year);
+  Coin (rhs:const Coin&);
+  friend std::ostream& operator= (rhs: const Coin& ): Coin&;
+  friend std::ostream& operator<<(ostream& ost, const Coin& coin);
+  std::string add_note(s);
+  friend std::istream& operator>>(istream& ist, Coin& coin);
+
+};
