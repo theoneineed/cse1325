a3d7c1f8a7e6db4c9a44b2b1280904050a9fe3cd
Nabin <nabinbabu2015@gmail.com>
2020-03-28 02:59:01 -0500

    P07 starts, entry to GUI

---
+++ elsa/sprint2/mainwin.h
@@ -0,0 +1,56 @@
+#ifndef _MAINWIN_H
+#define _MAINWIN_H
+
+#include <gtkmm.h>
+#include "store.h"
+
+public:
+  Mainwin();
+  virtual ~Mainwin();
+
+protected:
+  void on_quit_click();
+  void on_view_peripheral_click();
+  void on_view_desktop_click();
+  void on_view_order_click();
+  void on_view_customer_click();
+  void on_insert_peripheral_click();
+  void on_insert_desktop_click();
+  void on_insert_order_click();
+  void on_insert_customer_click();
+  void on_about_click();
+  std::string get_string(std::string prompt);
+  double get_double(std::string prompt);
+  int get_int(std::string prompt);
+  void set_data(std::string s);
+  void set_msg(std::string s);
+
+private:
+  Store* store;
+  Gtk::Label* data;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
