f96d370ba87f6cf6859b5896f872db5eb11a79bd
Nabin <nabinbabu2015@gmail.com>
2020-04-21 06:47:57 -0500

    edit1

--- elsa/sprint4/src/ram.cpp
+++ elsa/sprint4/src/ram.cpp
@@ -1,13 +1,41 @@
 #include "ram.h"
-
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <ostream>
   Ram::Ram(std::string name, double const, int gb)
-  {
-
-  }
+  {  }
 
   Ram:: ~Ram(){}
 
-  Ram::Ram(std::istream& ist)
+  Ram::Ram(std::istream& ist)::Options(ist)
   {
-    
+    getline(ist,_name);
+    ist>>_cost;
+    ist.tgnore();
   }
+
+std::string to_string(int n)
+{
+  switch(n)
+  {
+    case 1: return "1 GB";
+    case 2: return "2 GB";
+    case 3: return "3 GB";
+    case 4: return "4 GB";
+    case 5: return "5 GB";
+    case 6: return "6 GB";
+    default: return "Default Ram size";
+  }
+}
+
+std::ostream& operator<<(std::ostream& ost) {
+    ost << Ram.size();
+    return ost;
+}
+
+
+std::string Ram::gb1()
+{
+  return std::to_string(_gb);
+}
--- elsa/sprint4/src/ram.h
+++ elsa/sprint4/src/ram.h
@@ -4,11 +4,20 @@
 #include <string>
 #include <iostream>
 #include<fstream>
+#include "options.h"
+enum class Ram_subtypes
+{
+    onegb,twogb,threegb,fourgb,fivegb,sixgb
+};
+
 class Ram {
   public:
     Ram(std::string name, double const, int gb);
     virtual ~Ram();
     Ram(std::istream& ist);
+    std::string to_string() const override;
+    void save(std::ostream& ost) override;
+    std::string gb1();
   protected:
     int _gb;
 };
